e3dbffc1f29784f0c5a3b56a697928364b675692
qingyu0620 <187825990+qingyu0620@users.noreply.github.com>
2026-06-03 18:33:13 +0800

    icm42688p测试通过 + imu架构重构

--- thread/gpio/trd_gpio.cpp
+++ thread/gpio/trd_gpio.cpp
@@ -11,12 +11,10 @@
 
 #include "trd_gpio.hpp"
 #include "thread.hpp"
-#if CONFIG_MOD_DEV_WS2812B
-#include "ws2812b.hpp"
-#else
 #include "output.hpp"
+#include "timer.hpp"
 #include "input.hpp"
-#endif
+#include "zephyr/sys/printk.h"
 
 #define GPIO_GET(node_id)   GPIO_DT_SPEC_GET(DT_NODELABEL(node_id), gpios)
 
@@ -24,41 +22,30 @@ namespace thread::output {
 
 static Thread<2048> thread_{};
 
-#if CONFIG_MOD_DEV_WS2812B
-static Ws2812b led_alert{};
-#else
 static Output led_alert{};
-#endif
 
 static void led_cycle()
 {
-#if CONFIG_MOD_DEV_WS2812B
-
-    static uint8_t phase = 0;
-    static constexpr Ws2812b::Color colors[] {
-        {32, 0,  0 },
-        {0,  32, 0 },
-        {0,  0,  32},
-        {0,  0,  0 },
-    };
-    led_alert.set(colors[phase++ % ARRAY_SIZE(colors)]);
-
-#else
-
     led_alert.Toggle();
-    
-#endif
 }
 
 static void Task(void*, void*, void*)
 {
-    static constexpr uint32_t kPeriodMs = 500;
+    static constexpr uint32_t kPeriodMs = 1;
+    Timer timer(1000);
 
     for (;;)
     {
         const int64_t tick_start = k_uptime_get();
 
-        led_cycle();
+        timer.Update();
+        timer.Clock(([](){
+
+            printk("hello\n");
+
+        }));
+
+        // led_cycle();
 
         const int64_t elapsed = k_uptime_get() - tick_start;
         const int64_t remain  = static_cast<int64_t>(kPeriodMs) - elapsed;
@@ -70,12 +57,7 @@ static void Task(void*, void*, void*)
 
 void thread_init()
 {
-#if CONFIG_MOD_DEV_WS2812B
-    led_alert.init(HPM_GPIO0, 0, 10);
-    led_alert.set_color_order(Ws2812b::ColorOrder::RGB);
-#else
-    led_alert.init(GPIO_GET(led_alert));
-#endif
+    // led_alert.init(GPIO_GET(led_alert));
 }
 
 void thread_start(uint8_t prio)
